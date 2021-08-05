ea812cd3994258c491312b8cd634e08461fceb75
Joshua-Talltree <joshua.scott.talltree@gmail.com>
2021-08-04 23:05:26 -0500

    feat: working on building gateway props and the main for gateway service.

--- src/main/java/com/josh/zuulgatewayservice/ZuulGatewayServiceApplication.java
+++ src/main/java/com/josh/zuulgatewayservice/ZuulGatewayServiceApplication.java
@@ -2,6 +2,8 @@ package com.josh.zuulgatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+
 
 @SpringBootApplication
 public class ZuulGatewayServiceApplication {
@@ -10,4 +12,9 @@ public class ZuulGatewayServiceApplication {
 		SpringApplication.run(ZuulGatewayServiceApplication.class, args);
 	}
 
+	public WebMvcAutoConfiguration corsConfiguration() {
+		return new WebMvcAutoConfiguration() {
+
+		};
+	}
 }
